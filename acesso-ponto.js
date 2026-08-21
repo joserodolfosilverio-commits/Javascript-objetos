343a5347f99c29121841170e39a72f72d57c3cf3
joserodolfosilverio-commits <jose.rodolfo.silverio@escola.pr.gov.br>
2026-08-21 14:47:49 +0000

    679680

--- acesso-ponto.js
+++ acesso-ponto.js
@@ -4,8 +4,7 @@ const cliente = {
     idade: 32,
     cpf:"1122233345",
     email: "andre@dominio.com",
-}
-
+};
 console.log(
 `O nome do cliente é ${cliente.nome} e essa pessoa tem ${cliente.idade} anos.`
 );
