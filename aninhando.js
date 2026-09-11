28645623233b7cd4bd67a4df1d1b0cfaa8cace65
joserodolfosilverio-commits <jose.rodolfo.silverio@escola.pr.gov.br>
2026-09-11 14:13:42 +0000

    .

---
+++ aninhando.js
@@ -0,0 +1,16 @@
+const Cliente = {
+    nome:"João",
+    idade: 24, 
+    email: "joão@firma.com",
+    telefone: ["4488682718", "984626624"]
+};
+
+console.log (Cliente);
+
+Cliente.endereço = {
+    rua: "Rua Joseph Climber",
+    numero: 1337,
+    apartamento: true,
+    complemento: "ap 934",
+};
+
---
+++ tipo.js
@@ -0,0 +1,8 @@
+const Cliente = {
+    nome:"João",
+    idade: 24, 
+    email: "joão@firma.com",
+    telefone: ["4488682718", "984626624"]
+};
+
+console.log (Cliente);
